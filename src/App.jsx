6f4d992304870abf6e9d8fec50482fd720f91859
Packina <136891046+Packina@users.noreply.github.com>
2024-01-18 14:48:14 +0700

    Add:Edit activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
+import EditActivity from "./Components/EditActivity";
+
 
 function App() {
   return (
-    <>
-      <h1 className="text-pink-500">Hello World!</h1>
-    </>
+   <div>
+    <EditActivity/>
+   </div>
   );
 }
 
