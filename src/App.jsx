e3b2570e17ff526490a7c47808844ca6747a9528
Ohio <worrawutpanya@gmail.com>
2024-01-22 21:33:29 +0700

    finished: draft-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
+import NavMobile from "./components/NavMobile";
 
 function App() {
   return (
     <>
       <h1 className="text-pink-500">Hello World!</h1>
+      <NavMobile />
     </>
   );
 }
