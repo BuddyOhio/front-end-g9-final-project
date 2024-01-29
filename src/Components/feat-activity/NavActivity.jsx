e4808954010e98d292fe45638176b4be23ff5b37
opomp1 <bamiowen@gmail.com>
2024-01-29 16:17:38 +0700

    fix/edit-activity-card

--- src/Components/feat-activity/NavActivity.jsx
+++ src/Components/feat-activity/NavActivity.jsx
@@ -19,9 +19,21 @@ const NavActivity = () => {
           {activities.map((items, index) => (
             <div
               key={index}
-              className="text-gray-600 font-medium bg-[#ecfcff] pl-8 pr-4 py-2 flex flex-col relative rounded-full justify-center gap-1"
+              className="text-gray-600 font-medium bg-[#ecfcff] pl-[1.25rem] lg:pl-8 pr-4 py-2 flex flex-col relative rounded-full justify-center gap-1"
             >
-              <div className="absolute left-[-12%]  lg:w-[50px] xl:w-[65px] 2xl:left-[-10%]">
+              {/* Activity Image Logo | Trophy */}
+              <div
+                className={
+                  items.name === "Football" ||
+                  items.name === "Swimming" ||
+                  items.name === "Run" ||
+                  items.name === "Walk" ||
+                  items.name === "Hike" ||
+                  items.name === "Bicycle Ride"
+                    ? "absolute left-[-12%] w-[40px] lg:w-[50px] xl:w-[65px] 2xl:left-[-10%]"
+                    : "absolute left-[-25%] lg:left-[-18%] w-[80px] lg:w-[90px] xl:w-[120px] 2xl:left-[-15%]"
+                }
+              >
                 <img
                   src={
                     items.name === "Football"
@@ -36,23 +48,24 @@ const NavActivity = () => {
                       ? "../../../public/activity-hike.png"
                       : items.name === "Bicycle Ride"
                       ? "../../../public/activity-bicycle-ride.png"
-                      : "../../../public/activity.png"
+                      : "../../../public/activity-trophy.svg"
                   }
                   alt="trophie"
                   className="w-full h-full"
                 />
               </div>
+              {/* Activity: name / date / เฟือง edit */}
               <div className="flex justify-between items-center">
-                <div className="text-base xl:text-xl 2xl:text-2xl">
+                <div className="lg:text-base xl:text-xl 2xl:text-2xl">
                   {items.name}
                 </div>
-                <div className="flex justify-end gap-3">
+                <div className="flex justify-end md:gap-1 lg:gap-2 text-xs lg:text-sm xl:text-lg">
                   <h3>{items.date}</h3>
-                  <Link to="/card" className=" hover:scale-110">
+                  <Link to="/edit-activity" className=" hover:scale-110">
                     <img
                       src="../../../public/settings-gear-svgrepo-com.svg"
                       alt="clock"
-                      className="lg:w-4 xl:w-5"
+                      className="w-4 xl:w-5"
                     />
                   </Link>
                 </div>
@@ -61,18 +74,18 @@ const NavActivity = () => {
                 <div
                   className={
                     items.status === "Upcoming"
-                      ? "bg-[#ffd05b] px-1 py-0.5 text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
+                      ? "bg-[#ffd05b] px-1 py-0.5 text-[0.4rem] lg:text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
                       : items.status === "Ongoing"
-                      ? "bg-[#FF4545] px-1 py-0.5 text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
+                      ? "bg-[#FF4545] px-1 py-0.5 text-[0.4rem] lg:text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
                       : items.status === "Finished"
-                      ? "bg-[#0eb400] px-1 py-0.5 text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
-                      : "bg-gray-200 px-1 py-0.5 text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
+                      ? "bg-[#0eb400] px-1 py-0.5 text-[0.4rem] lg:text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
+                      : "bg-gray-200 px-1 py-0.5 text-[0.4rem] lg:text-[0.5rem] text-white font-bold rounded-full xl:text-xs"
                   }
                 >
                   {items.status}
                 </div>
-                <div className="flex justify-end gap-2 2xl:gap-3">
-                  <h3>{items.time}</h3>
+                <div className="flex justify-end gap-1 lg:gap-2 xl:text-lg 2xl:gap-3">
+                  <h3 className="hidden lg:block">{items.time}</h3>
                   <h3>{items.duration}</h3>
                   <img
                     src="../../../public/clock-regular.svg"
